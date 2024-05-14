d020f14e8f775292d832cc941f8feb2be1fc5966
Konstantin Raev <bestander@gmail.com>
2024-05-13 23:21:05 -0400

    duke runs

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 # sudo setcap 'cap_sys_nice=eip' ./eduke32
-# SDL_VIDEODRIVER=evdev 
-./eduke32 -l1 --led-gpio-mapping=adafruit-hat --led-rows=64 --led-cols=64 --led-chain=2 --led-pixel-mapper="U-mapper" --led-slowdown-gpio=3 --led-pwm-bits=10 --led-pwm-lsb-nanoseconds=500 --led-pwm-dither-bits=2 $@
+SDL_VIDEODRIVER=evdev ./eduke32 -l1 --led-gpio-mapping=adafruit-hat --led-rows=32 --led-cols=64 --led-chain=2 --led-pixel-mapper="U-mapper" --led-slowdown-gpio=3 --led-pwm-bits=10 --led-pwm-lsb-nanoseconds=500 --led-pwm-dither-bits=2 $@
--- src/duke_matrix_sdl_integration.cpp
+++ src/duke_matrix_sdl_integration.cpp
@@ -6,32 +6,24 @@ using namespace rgb_matrix;
 
 RGBMatrix *matrix;
 FrameCanvas *offscreen_canvas;
-Color color;
-Font font;
+rgb_matrix::Color color;
+rgb_matrix::Font font;
 
 int matrix_width, matrix_height;
 int surface_width, surface_height;
+int surface_to_matrix_ratio;
 
 void SDL_on_Init(int argc, char *argv[])
 {
     RGBMatrix::Options matrix_options;
     RuntimeOptions runtime_opt;
 
-    // // TODO move some to argv
-    // matrix_options.cols = matrix_width;
-    // matrix_options.rows = matrix_height;
-    // // matrix_options.chain_length = 2;
-    // // matrix_options.pixel_mapper_config = "U-mapper";
-    // matrix_options.pwm_lsb_nanoseconds = 500;
-    // matrix_options.pwm_dither_bits = 2;
-    // matrix_options.pwm_bits = 10;
-    // matrix_options.hardware_mapping = "adafruit-hat";
-    // runtime_opt.gpio_slowdown = 3;
     ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt);
-    printf("?????? PARAMS col %d row %d mapping %s \n", matrix_options.cols, matrix_options.rows, matrix_options.hardware_mapping);
-    surface_width = matrix_width = matrix_options.cols;
-    matrix_height = matrix_options.rows;
-    surface_height = matrix_width * 0.75;
+    matrix_width = 64;
+    surface_width = 640;
+    matrix_height = 64;
+    surface_height = 480;
+    surface_to_matrix_ratio = surface_width / matrix_width;
 
     matrix = RGBMatrix::CreateFromOptions(matrix_options, runtime_opt);
     if (matrix == NULL)
@@ -59,39 +51,47 @@ void SDL_on_Init(int argc, char *argv[])
              0);
 }
 
+void SDL_OverrideResolution(int *width, int *height)
+{
+    *width = surface_width;
+    *height = surface_height;
+}
+
 bool is_game_sleeping = false;
 
 void SDL_on_DrawFrame(uint32_t *pixels)
 {
-    // printf("drawing frame ");
-    // uint32_t *pix = pixels;
-    // for (int y = 0; y < matrix_height; ++y)
-    // {
-    //     for (int x = 0; x < matrix_width; ++x)
-    //     {
-    //         if (x < surface_width && y < surface_height)
-    //         {
-    //             if (!is_game_sleeping)
-    //             {
-    //                 uint8_t r = *pix >> 16;
-    //                 uint8_t g = *pix >> 8;
-    //                 uint8_t b = *pix;
-    //                 // printf("%d %d %d %d %d \n", x, y, r, g, b);
-    //                 offscreen_canvas->SetPixel(x, y, r, g, b);
-    //             }
-    //             else
-    //             {
-    //                 offscreen_canvas->SetPixel(x, y, 0, 0, 0);
-    //             }
-    //             pix = pix + 10;
-    //         }
-    //         else
-    //         {
-    //             offscreen_canvas->SetPixel(x, y, 0, 0, 0);
-    //         }
-    //     }
-    // }
-     DrawText(offscreen_canvas, font,
+    uint32_t *pix = pixels;
+    for (int y = 0; y < matrix_height; ++y)
+    {
+        for (int x = 0; x < matrix_width; ++x)
+        {
+            if (!is_game_sleeping && y < surface_height / 10)
+            {
+                uint16_t red = 0;
+                uint16_t green = 0;
+                uint16_t blue = 0;
+                int ratio_square = surface_to_matrix_ratio * surface_to_matrix_ratio;
+
+                for (int ix = 0; ix < surface_to_matrix_ratio; ++ix)
+                {
+                    for (int iy = 0; iy < surface_to_matrix_ratio; ++iy)
+                    {
+                        pix = pixels + x * surface_to_matrix_ratio + ix + (surface_to_matrix_ratio * y + iy) * surface_width;
+                        red += *pix >> 16 & 0xFF;
+                        green += *pix >> 8 & 0xFF;
+                        blue += *pix & 0xFF;
+                    }
+                }
+                offscreen_canvas->SetPixel(x, y, red / ratio_square, green / ratio_square, blue / ratio_square);
+            }
+            else
+            {
+                offscreen_canvas->SetPixel(x, y, 0, 0, 0);
+            }
+        }
+    }
+    DrawText(offscreen_canvas, font,
              0, 60,
              color, NULL, "TEST",
              1);
